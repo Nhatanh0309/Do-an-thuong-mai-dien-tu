b571f1b7c0483d06ea69332251eca3f8741a0f64
Nhatanh0309 <129145986+Nhatanh0309@users.noreply.github.com>
2026-01-20 17:07:53 +0700

    Delete Models directory

--- Models/Brand.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAdmin.Models;
-
-public class Brand
-{
-    public short BrandId{get; set;}
-    public string BrandName{get;set;} = null!;
-    public string SupplierId{get;set;} = null!;
-    public string Description{get;set;} = null!;
-}
